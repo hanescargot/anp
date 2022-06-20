25e394909ed70bb098858780d417c9d561b5ad6d
Hyunj Han <hyunjhan@Hyunjui-MacBookPro.local>
2022-06-21 02:53:15 +0900

    init

---
+++ android/app/src/main/kotlin/com/dpyrion/anp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dpyrion.anp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:anp/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
